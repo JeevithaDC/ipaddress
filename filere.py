e46332fc58f842950ca9d144abc70a95be500513
JeevithaDC <49547813+JeevithaDC@users.noreply.github.com>
2019-05-21 12:11:14 +0530

    Update filere.py

--- filere.py
+++ filere.py
@@ -2,7 +2,7 @@ import re
 # s=open("filere.txt",'r')
 # line=s.read()
 #pat=raw_input("enter the patterns to search: ")
-line="PATTERN HOW jaVa 10.2.3.2 30.2.1.2 11111 12 0.0.0.1 345.1.1.1"
+line=intput("enter the input in which we have to find pattern")
 pat= r"\b((?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.\b(?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.\b(?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.\b(?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))\b"
 result=re.findall(pat,line)
 print result
