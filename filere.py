cc33750a3b6b043d2ae78f6c074179da280c6295
JeevithaDC <49547813+JeevithaDC@users.noreply.github.com>
2019-05-21 12:00:28 +0530

    Add files via upload

---
+++ filere.py
@@ -0,0 +1,10 @@
+import re 
+s=open("filere.txt",'r')
+line=s.read()
+#pat=raw_input("enter the patterns to search: ")
+pat= r"\b((?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.\b(?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.\b(?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\.\b(?:1?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))\b"
+result=re.findall(pat,line)
+print result
+
+
+
